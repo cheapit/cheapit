eb3c12b7ab885cf3617392b7e67f97171d9206de
meir peretz <44440942+meirpertzz@users.noreply.github.com>
2023-12-03 16:38:54 -0800

    update p

---
+++ nhost/migrations/default/1701650323630_alter_table_public_chain_data_instructions_add_unique_chain_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."chain_data_instructions" drop constraint "chain_data_instructions_chain_id_key";
---
+++ nhost/migrations/default/1701650323630_alter_table_public_chain_data_instructions_add_unique_chain_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."chain_data_instructions" add constraint "chain_data_instructions_chain_id_key" unique ("chain_id");
